4a46d52545566da58f2cf9cd12836623b68f93fb
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 01:12:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,6 +4,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game
-   "Jay-Z is better than Beyonce"
+def play_game(player1, player2)
+   "#{player1} is better than #{player2}"
 end
