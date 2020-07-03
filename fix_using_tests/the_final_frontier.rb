f37eaadff33d798f8a3a6e131df68e7deffb9728
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 01:10:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,7 +11,7 @@ def state_log(star_date)
   puts "Captain's Log, star date #{star_date}."
 end
 
-def engage
+def engage(date)
   date = generate_star_date
   puts "Captain's Log, star date #{star_date}."
 end
