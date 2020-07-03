548d1caa6e4754a573f2e1be0551ecacb19ba511
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 00:56:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 require 'pry' # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{player2}"
+  "Hello #{player1} & #{playar2}"
 end
-binding.pry
+
 def play_game(player2)
-  "Jay-Z is better than #{player2}"
+  "#{player1} is better than #{player2}"
 end
