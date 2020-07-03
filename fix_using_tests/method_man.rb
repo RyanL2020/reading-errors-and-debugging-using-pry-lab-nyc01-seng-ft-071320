ddf20151625ff861e39b8c210d5f9ae1ba74fc03
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 00:55:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
-# don't forget to add: require 'pry'
+require 'pry' # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{player2}"
 end
-
+binding.pry
 def play_game(player2)
-  "#{player1} is better than #{player2}"
+  "Jay-Z is better than #{player2}"
 end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,17 @@
 # don't forget to add: require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
+def generate_star_date(star_date)
+  star_date = 45410.6
+  #(rand(100000) + 400000) / 10.0
 end
+
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  puts "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts "Captain's Log, star date #{star_date}."
 end
