ad7fe4c3d1fc3422fc0c89b8e371fe8612e53067
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 01:21:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,17 +1,16 @@
 require 'pry' # don't forget to add: require 'pry'
 
-def generate_star_date(star_date)
-  (rand(100000) + 400000) / 10.0
-end
+#def generate_star_date(star_date)
+#  (rand(100000) + 400000) / 10.0
+#end
 
 
 
-def state_log(star_date)
-  binding.pry
-  puts "Captain's Log, star date #{star_date}."
-end
+#def state_log(star_date)
+#  puts "Captain's Log, star date #{star_date}."
+#end
 
-def engage(star_date)
-  date = generate_star_date
-  puts "Captain's Log, star date #{star_date}."
-end
+#def engage(star_date)
+#  date = generate_star_date
+#  puts "Captain's Log, star date #{star_date}."
+#end
